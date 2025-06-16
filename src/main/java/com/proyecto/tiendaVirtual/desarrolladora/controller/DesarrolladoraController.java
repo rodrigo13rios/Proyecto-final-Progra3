ce05f0311c847e95755a808775756b41b84f6d54
SebastianKloster <sk8.kloster@gmail.com>
2025-06-16 19:08:34 -0300

    Pequeños cambios al Controller de Desarrolladora, y al Create del DesarrolladoraService

--- src/main/java/com/proyecto/tiendaVirtual/desarrolladora/controller/DesarrolladoraController.java
+++ src/main/java/com/proyecto/tiendaVirtual/desarrolladora/controller/DesarrolladoraController.java
@@ -2,6 +2,7 @@ package com.proyecto.tiendaVirtual.desarrolladora.controller;
 
 import com.proyecto.tiendaVirtual.desarrolladora.model.Desarrolladora;
 import com.proyecto.tiendaVirtual.desarrolladora.service.DesarrolladoraService;
+import com.proyecto.tiendaVirtual.exceptions.ElementoNoEncontradoException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,23 +15,35 @@ public class DesarrolladoraController {
     @Autowired
     private DesarrolladoraService service;
 
-    @GetMapping("/get")
+//    Get ALL
+    @GetMapping
     public List<Desarrolladora> getAll() {
         return service.getAll();
     }
 
-    @PostMapping("/create")
+//    Get By ID
+    @GetMapping("/{id}")
+    public ResponseEntity<Desarrolladora> getById(@PathVariable Long id) {
+        Desarrolladora desarrolladora = service.findById(id)
+                .orElseThrow(() -> new ElementoNoEncontradoException("No se encuentra una desarrolladora con ese ID"));
+        return ResponseEntity.ok(desarrolladora);
+    }
+
+//    Create
+    @PostMapping
     public ResponseEntity<Desarrolladora> create(@RequestBody Desarrolladora desarrolladora) {
         Desarrolladora result = service.create(desarrolladora);
         return new ResponseEntity<>(result, HttpStatus.CREATED);
     }
 
+//    Update
     @PutMapping("/{id}")
     public ResponseEntity<Desarrolladora> update(@PathVariable Long id, @RequestBody Desarrolladora desarrolladora) {
         Desarrolladora result = service.update(id,desarrolladora);
         return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
+//    Delete
     @DeleteMapping("/{id}")
     public void delete(@PathVariable Long id) {
         service.delete(id);
