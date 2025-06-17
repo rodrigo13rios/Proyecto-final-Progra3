b155b75ab022df4deb7d954a84824bd9b9c79fa4
SebastianKloster <sk8.kloster@gmail.com>
2025-06-17 02:11:08 -0300

    Arreglé el SecurityConfig Ahora la autentificacion funciona, aunque elimine todas las rutas mientras testeaba, ahora es solo cuestion de agregarlas

--- src/main/java/com/proyecto/tiendaVirtual/config/SecurityConfig.java
+++ src/main/java/com/proyecto/tiendaVirtual/config/SecurityConfig.java
@@ -5,8 +5,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -14,35 +18,32 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
     @Autowired
     private UserServiceImpl userService;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+        return http.getSharedObject(AuthenticationManagerBuilder.class)
+                .userDetailsService(userService)
+                .passwordEncoder(passwordEncoder)
+                .and()
+                .build();
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http)throws Exception{
         return http
                 .csrf(csrf->csrf.disable())
                 .authorizeHttpRequests(auth->auth
-                        .requestMatchers(HttpMethod.POST,"/api/users/create")
+
+                        .requestMatchers("/api/test/**").authenticated()
+
+                        .requestMatchers(HttpMethod.POST,"/api/users")
                                         .permitAll()
-                        .requestMatchers("/api/users/**")
-                                        .authenticated()
-                        .requestMatchers("/api/juego/get",
-                                         "/api/juego/buscarPorId"
-                                        ,"/api/juego/buscarPorNombre")
-                                        .permitAll()
-                        .requestMatchers("/api/juegos/create",
-                                         "/api/juegos/update",
-                                         "/api/juegos/delete")
-                                        .hasRole("DESARROLLADORA")
-                        .requestMatchers("/api/perfil/**")
-                                        .hasRole("PERFIL")
-                        .requestMatchers("/api/billetera/**")
-                                        .hasRole("PERFIL")
-                        .requestMatchers("/api/desarrolladora/**")
-                                        .hasRole("DESARROLLADORA")
                         .anyRequest().authenticated()
 
-                ).build();
-        //La parte de crear usuario, ver los juegos son de acceso publico.
-        //La creacion, actualizacion y borrar los videojuegos solo desde el rol DESARROLLADORA
-        //Lo que es el perfil y la billetera es accesible desde el rol Perfil.
+                )
+                .httpBasic(Customizer.withDefaults())
+                .build();
     }
 }
