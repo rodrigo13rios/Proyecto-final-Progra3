3a0d4edfaff5f69b74dd505fbda5db5a25191cd6
rodrigo13rios <riosrodrigo9@gmail.com>
2025-11-24 17:13:44 -0300

    Cambios para la lista de favoritos

--- src/main/java/com/proyecto/tiendaVirtual/config/SecurityConfig.java
+++ src/main/java/com/proyecto/tiendaVirtual/config/SecurityConfig.java
@@ -40,7 +40,7 @@ public class SecurityConfig {
         CorsConfiguration config = new CorsConfiguration();
 
         config.setAllowedOrigins(List.of("http://localhost:4200")); // Angular
-        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE","PATCH", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
 
