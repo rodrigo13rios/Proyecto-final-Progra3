f23a1ce946a2d644ec0ccb36f5284171e8c9fe00
SebastianKloster <sk8.kloster@gmail.com>
2026-02-09 19:58:00 -0300

    Save incompleto

--- src/main/java/com/proyecto/tiendaVirtual/config/SecurityConfig.java
+++ src/main/java/com/proyecto/tiendaVirtual/config/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
@@ -97,7 +98,9 @@ public class SecurityConfig {
 
                         .anyRequest().authenticated()
                 )
-                .httpBasic(Customizer.withDefaults())
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
                 .build();
     }
 }
