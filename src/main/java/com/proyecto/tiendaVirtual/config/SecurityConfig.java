f1757f975acce9bee2335aa68b5d116ea6650ca8
rodrigo13rios <riosrodrigo9@gmail.com>
2026-02-12 17:08:15 -0300

    cambio las direcciones a https para poder integrar mercado pago

--- src/main/java/com/proyecto/tiendaVirtual/config/SecurityConfig.java
+++ src/main/java/com/proyecto/tiendaVirtual/config/SecurityConfig.java
@@ -58,7 +58,7 @@ public class SecurityConfig {
 
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(List.of("http://localhost:4200"));
+        config.setAllowedOrigins(List.of("http://localhost:4200", "https://localhost:4200"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
